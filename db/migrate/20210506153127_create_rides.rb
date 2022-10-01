92f7e9a23c593c9b03d1ec7ccf6b424b6e47f56e
OnanThairu <thairuonan@gmail.com>
2022-10-01 21:29:50 +0300

    commit tree 6fad9d7e4558183c6b6df4a7ff36361841974155

--- db/migrate/20210506153127_create_rides.rb
+++ db/migrate/20210506153127_create_rides.rb
@@ -1,7 +1,9 @@
 class CreateRides < ActiveRecord::Migration[6.1]
   def change
     create_table :rides do |t|
-
+      t.integer "taxi_id", null: false
+      t.integer "passenger_id", null: false
+      
       t.timestamps
     end
   end
